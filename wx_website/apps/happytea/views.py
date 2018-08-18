60befa353e31ce3af63d623b4f2974c16495904d
nefeithu <hxq_7324@qq.com>
2018-08-18 18:07:06 +0800

    简单消息处理

--- wx_website/apps/happytea/views.py
+++ wx_website/apps/happytea/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def wxmp(request):
+    if request.method == 'GET':
+        echostr = request.GET.get('echostr')
+        logger.info('wx reg succ')
+        return HttpResponse(echostr)
